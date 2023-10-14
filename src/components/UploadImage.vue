f00fef533b78b5c89bdc9afc8c442bdc4cbee0a9
Talich12 <denis.tal@mail.ru>
2023-10-14 17:39:17 +1000

    feat: Добавиь props в компонент UploadImage

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -8,6 +8,7 @@
 <script>
 import axios from 'axios'
 export default {
+    props: ['mode'],
     methods: {
         handleBrowseClick() {
             document.getElementById('fileInput').click();
@@ -17,19 +18,36 @@ export default {
             let formData = new FormData();
             formData.append('file', file);
 
-            axios.post('http://10.193.141.30:3000/face-recognition', formData, {
-                headers: {
-                'Content-Type': 'multipart/form-data'
-                }
-            })
-            .then(response => {
-                // Обработка успешной загрузки
-                console.log(response);
-            })
-            .catch(error => {
-                // Обработка ошибки
-                console.log(error);
-            });
+            if (this.mode == "face-recognition"){
+                axios.post('http://10.193.141.30:3000/face-recognition', formData, {
+                    headers: {
+                    'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then(response => {
+                    // Обработка успешной загрузки
+                    console.log(response);
+                })
+                .catch(error => {
+                    // Обработка ошибки
+                    console.log(error);
+                });
+            }
+            else if (this.mode == "people-count"){
+                axios.post('http://10.193.141.30:3000/people-count', formData, {
+                    headers: {
+                    'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then(response => {
+                    // Обработка успешной загрузки
+                    console.log(response);
+                })
+                .catch(error => {
+                    // Обработка ошибки
+                    console.log(error);
+                });
+            }
         }
     }
 }
