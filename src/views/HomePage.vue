6776853007f1c4813b6897184accf9a77582fcd6
SawMassacre <codemassacre@outlook.com>
2023-10-15 12:23:53 +1000

    feat: Started interaction block

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
     <header class="header">
-      <img style="height: 3rem" src="../assets/logofull.svg" alt="Logo" />
+      <img
+        style="height: 3rem; z-index: 10"
+        src="../assets/logofull.svg"
+        alt="Logo"
+      />
     </header>
     <div style="width: 100vw; height: 100vh">
       <InteractionBlock></InteractionBlock>
+      <div class="center">
+        <vs-select placeholder="Select" v-model="value">
+          <vs-option label="Vuesax" value_select="1"> Vuesax </vs-option>
+          <vs-option label="Vue" value_select="2"> Vue </vs-option>
+          <vs-option label="Javascript" value_select="3">
+            Javascript
+          </vs-option>
+          <vs-option disabled label="Sass" value_select="4"> Sass </vs-option>
+          <vs-option label="Typescript" value_select="5">
+            Typescript
+          </vs-option>
+          <vs-option label="Webpack" value_select="6"> Webpack </vs-option>
+          <vs-option label="Nodejs" value_select="7"> Nodejs </vs-option>
+        </vs-select>
+      </div>
       <h2 style="margin-top: 2vh">Удобства</h2>
       <ComfortBlock class="animate__animated animate__bounce"></ComfortBlock>
       <h2>Помещения</h2>
@@ -32,6 +51,9 @@ export default {
     OrderBLock,
     InteractionBlock,
   },
+  data: () => ({
+    value_select: "",
+  }),
 };
 </script>
 
